e57bee603e654a588e819762936988a73bf71c26
Ivan <designbu-ivko@mail.ru>
2020-08-31 23:32:01 +0300

    release 23:31

--- src/App.js
+++ src/App.js
@@ -5,54 +5,54 @@ import ProductForm from './components/product-form/product-form';
 import Background from './components/img/background.png';
 
 
-
 function App(props) {
-  let [products, setProducts]=React.useState([
-  {
-    id:1,
-    name:"Нямушка",
-    ingredient: "с фуа-гра",
-    size: 10,
-    bonus: "мышь в подарок",
-    weight: "0,5",
-    selected: false,
-    disabled: false,
-    description: "Печень утки разварная с артишоками."
-  },
-  {
-    id:2,
-    name:"Нямушка",
-    ingredient: "с рыбой",
-    size: 40,
-    bonus: "2 мыши в подарок",
-    weight: 2,
-    selected: false,
-    disabled: false,
-    description: "Головы щучьи с чесноком да свежайшая сёмгушка."
-  },
-  {
-    id: 3,
-    name:"Нямушка",
-    ingredient: "с курой",
-    size: 100,
-    bonus: "5 мышей в подарок заказчик доволен",
-    weight: 5,
-    selected: false,
-    disabled: true,
-    description: "Филе из цыплят с трюфелями в бульоне."
-  }
-])
+  let [products, setProducts] = React.useState([
+    {
+      id: 1,
+      name: "Нямушка",
+      ingredient: "с фуа-гра",
+      size: 10,
+      bonus: "мышь в подарок",
+      weight: "0,5",
+      selected: false,
+      disabled: false,
+      description: "Печень утки разварная с артишоками."
+    },
+    {
+      id: 2,
+      name: "Нямушка",
+      ingredient: "с рыбой",
+      size: 40,
+      bonus: "2 мыши в подарок",
+      weight: 2,
+      selected: false,
+      disabled: false,
+      description: "Головы щучьи с чесноком да свежайшая сёмгушка."
+    },
+    {
+      id: 3,
+      name: "Нямушка",
+      ingredient: "с курой",
+      size: 100,
+      bonus: "5 мышей в подарок заказчик доволен",
+      weight: 5,
+      selected: false,
+      disabled: true,
+      description: "Филе из цыплят с трюфелями в бульоне."
+    }
+  ])
 
-function OnClick(id) {
-  setProducts (
-    products.map(product => {
-      if (id===product.id && product.disabled===false) {
-        product.selected =! product.selected;
-      }
-      return product
-    })
-  )
-}
+  function OnClick(id) {
+    setProducts(
+      products.map(product => {
+        if (id === product.id && product.disabled === false) {
+          product.selected = !product.selected;
+        }
+        return product
+      })
+    )
+  }
+
   return (
     <div className="wrapper">
       <img className="background"/>
